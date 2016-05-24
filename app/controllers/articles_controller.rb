fe648d716211cde6a2c70870c341d0f2a8a583ee
jeffthemaximum <frey.maxim@gmail.com>
2016-05-24 13:31:05 -0400

    cruddy mccrudface

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+
+        @article = Article.new(params.require(:article).permit(:title, :text))
+        
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :text))
+            redirect_to action: "show", id: @article.id
+        else
+            render 'edit'
+        end
+    end
+end
